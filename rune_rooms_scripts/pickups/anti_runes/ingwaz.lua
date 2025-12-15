ddfc18840c789565eae30dcb1f87a155729d3346
BrakeDude <lockbrake@gmail.com>
2025-12-15 23:12:16 +0300

    Othala tweaks + Rare Chests compatibility

--- rune_rooms_scripts/pickups/anti_runes/ingwaz.lua
+++ rune_rooms_scripts/pickups/anti_runes/ingwaz.lua
@@ -7,41 +7,46 @@ function IngwazRune:UseIngwaz(ingwaz, player, useflags)
 	local entities = Isaac.GetRoomEntities()
 	local rng = player:GetCardRNG(ingwaz)
 	RuneRooms.Helpers:PlayGiantBook("Ingwaz", RuneRooms.Enums.SoundEffect.RUNE_INGWAZ, player, rng)
+	local addInvFrames = false
 	for i = 1, #entities do
-        if entities[i]:ToPickup() then
+		if entities[i]:ToPickup() then
 			local pickup = entities[i]:ToPickup()
 			if TSIL.Pickups.IsChest(pickup) then
 				pickup:TryOpenChest(player)
-            else
-
+			else
 			end
 			if RepentancePlusMod then
-				if entities[i].Variant == RepentancePlusMod.CustomPickups.FLESH_CHEST then
-					RepentancePlusMod.openFleshChest(entities[i])
-				elseif entities[i].Variant == RepentancePlusMod.CustomPickups.SCARLET_CHEST then
-					RepentancePlusMod.openScarletChest(entities[i])
-				elseif entities[i].Variant == RepentancePlusMod.CustomPickups.BLACK_CHEST then
-					RepentancePlusMod.openBlackChest(entities[i])
+				if pickup.Variant == RepentancePlusMod.CustomPickups.FLESH_CHEST then
+					RepentancePlusMod.openFleshChest(pickup)
+				elseif pickup.Variant == RepentancePlusMod.CustomPickups.SCARLET_CHEST then
+					RepentancePlusMod.openScarletChest(pickup)
+				elseif pickup.Variant == RepentancePlusMod.CustomPickups.BLACK_CHEST then
+					RepentancePlusMod.openBlackChest(pickup)
 				end
 			end
-            if RareChests then
-                if entites[i].Variant == CARDBOARD_CHEST then
-                    RareChests.openCardboardChest(pickup, player)
-                elseif entites[i].Variant == FILE_CABINET then
-                    RareChests.openFileCabinet(pickup, player)
-                elseif entites[i].Variant == SLOT_CHEST then
-
-                elseif entites[i].Variant == TOMB_CHEST then
-                    RareChests.openTombChest(pickup)
-                elseif entites[i].Variant == DEVIL_CHEST then
-                    RareChests.openDevilChest(pickup, player)
-                elseif entites[i].Variant == CURSED_CHEST then
-                     RareChests.openCursedChest(pickup, player)
-                elseif entites[i].Variant == BLOOD_CHEST then
-                elseif entites[i].Variant == PENITENT_CHEST then
-
-                end
-            end
+			if RareChests then
+				if pickup.Variant == CARDBOARD_CHEST then
+					RareChests.openCardboardChest(pickup, player)
+				elseif pickup.Variant == FILE_CABINET then
+					RareChests.openFileCabinet(pickup, player)
+				elseif pickup.Variant == SLOT_CHEST then
+					RareChests.openCursedChest(pickup, player)
+				elseif pickup.Variant == TOMB_CHEST then
+					RareChests.openTombChest(pickup)
+				elseif pickup.Variant == DEVIL_CHEST then
+					RareChests.openDevilChest(pickup, player)
+				elseif pickup.Variant == CURSED_CHEST then
+					if not addInvFrames then
+						addInvFrames = true
+						player:AddCollectibleEffect(CollectibleType.COLLECTIBLE_BOOK_OF_SHADOWS, false, 1, true)
+					end
+					RareChests.openCursedChest(pickup, player)
+				elseif pickup.Variant == BLOOD_CHEST then
+					RareChests.openBloodChest(pickup)
+				elseif pickup.Variant == PENITENT_CHEST then
+					RareChests.openPenitentChest(pickup)
+				end
+			end
 		end
 	end
 
--- rune_rooms_scripts/pickups/anti_runes/othala.lua
+++ rune_rooms_scripts/pickups/anti_runes/othala.lua
@@ -12,11 +12,12 @@ function OthalaRune:UseOthala(othala, player, useflags)
 	local history = player:GetHistory()
 
 	local itemsTable = history:GetCollectiblesHistory()
+	if #itemsTable == 0 then return end
 	local index, item, collectConfig
 	local itemsToGet = RuneRooms.Helpers:HasMagicChalk(player) and 2 or 1
 	for _ = 1, itemsToGet do
 		repeat
-			index = rng:RandomInt(1, rng:RandomInt(1, #itemsTable))
+			index = rng:RandomInt(1, #itemsTable)
 			item = itemsTable[index]:GetItemID()
 			collectConfig = itemConfig:GetCollectible(item)
 			if
@@ -29,38 +30,11 @@ function OthalaRune:UseOthala(othala, player, useflags)
 			end
 		until index or #itemsTable == 0
 		if index then
-			if not data.OthalaClone then
-				data.OthalaClone = {}
-			end
-			table.insert(data.OthalaClone, item)
+			player:AnimateCollectible(item, "UseItem", "PlayerPickup")
+			player:QueueItem(Isaac.GetItemConfig():GetCollectible(item))
+			SFXManager():Play(SoundEffect.SOUND_POWERUP1, 1, 0)
 		end
 		index = nil
 	end
 end
 RuneRooms:AddCallback(ModCallbacks.MC_USE_CARD, OthalaRune.UseOthala, RuneRooms.Enums.Runes.OTHALA)
-
----@param player EntityPlayer
----@return boolean
-local function PickingUp(player)
-	local s = player:GetSprite()
-	if s:GetAnimation():sub(1, 8) == "PickWalk" then
-		return true
-	end
-	return false
-end
-
-function OthalaRune:OthalaDuplicatePickup(player)
-	local data = player:GetData()
-	if data.OthalaClone then
-		if not PickingUp(player) and not player.QueuedItem.Item then
-			player:AnimateCollectible(data.OthalaClone[1], "UseItem", "PlayerPickup")
-			SFXManager():Play(SoundEffect.SOUND_POWERUP1, 1, 0)
-			player:QueueItem(Isaac.GetItemConfig():GetCollectible(data.OthalaClone[1]))
-			table.remove(data.OthalaClone, 1)
-		end
-		if #data.OthalaClone == 0 then
-			data.OthalaClone = nil
-		end
-	end
-end
-RuneRooms:AddCallback(ModCallbacks.MC_POST_PEFFECT_UPDATE, OthalaRune.OthalaDuplicatePickup)
