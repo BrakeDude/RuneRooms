8d44cbfe873c0ad7c4f4ea57928e2894483be671
BrakeDude <lockbrake@gmail.com>
2025-12-30 14:58:42 +0300

    Now places rune rooms as an extra one instead of replacing normal room

--- rune_rooms_scripts/room/levelgen.lua
+++ rune_rooms_scripts/room/levelgen.lua
@@ -1,4 +1,5 @@
 local LevelGen = {}
+local game = Game()
 ---Adds a new rune room. The room id must be unique and it has to be a chest.
 ---
 ---This function must be called before MC_PRE_LEVEL_PLACE_ROOM callback fires, otherwise the room won't be loaded.
@@ -28,30 +29,45 @@ function RuneRooms:AddToRoomSpawnChance(value)
 	RuneRooms:SetRoomSpawnChance(currentChance + value)
 end
 
----@param levelGeneratorRoom LevelGeneratorRoom
----@param roomConfigRoom RoomConfigRoom
----@param seed integer
----@return RoomConfigRoom
-function LevelGen:ReplaceRoom(levelGeneratorRoom, roomConfigRoom, seed)
+function LevelGen:PlaceRoom()
+	local level = game:GetLevel()
+	if
+		game:IsGreedMode()
+		or game:GetStateFlag(GameStateFlag.STATE_BACKWARDS_PATH)
+		or level:GetAbsoluteStage() >= LevelStage.STAGE4_1
+	then
+		RuneRooms:SetRoomSpawnChance(0)
+		return
+	end
+	local seed = level:GetDungeonPlacementSeed()
 	local rng = RNG(seed)
-	if rng:RandomFloat() <= RuneRooms:GetRoomSpawnChance() then
-		if levelGeneratorRoom:IsDeadEnd()
-		and roomConfigRoom.Shape == RoomShape.ROOMSHAPE_1x1
-		and roomConfigRoom.Type == RoomType.ROOM_DEFAULT then
-			local outcome = WeightedOutcomePicker()
-			for roomID, weight in pairs(RuneRooms.Constants.RUNE_ROOMS_IDS) do
-				outcome:AddOutcomeFloat(roomID, weight)
-			end
-			local roomID = outcome:PickOutcome(rng)
-			local roomconf = RoomConfig.GetRoomByStageTypeAndVariant(StbType.SPECIAL_ROOMS, RoomType.ROOM_CHEST, roomID, 0)
-			RuneRooms:SetRoomSpawnChance(-0.1)
-			return roomconf
+	if RuneRooms:GetRoomSpawnChance() <= rng:RandomFloat() then
+		local outcome = WeightedOutcomePicker()
+		for roomID, weight in pairs(RuneRooms.Constants.RUNE_ROOMS_IDS) do
+			outcome:AddOutcomeFloat(roomID, weight)
 		end
+		local roomID = outcome:PickOutcome(rng)
+		local roomconf = RoomConfig.GetRoomByStageTypeAndVariant(StbType.SPECIAL_ROOMS, RoomType.ROOM_CHEST, roomID, 0)
+		local options = level:FindValidRoomPlacementLocations(roomconf, -1, false, false)
+		for _, gridIndex in pairs(options) do
+			local canPlace = true
+			local neighbors = level:GetNeighboringRooms(gridIndex, roomconf.Shape)
+			local shuffled = RuneRooms.Helpers:Shuffle(neighbors, rng)
+			for doorSlot, neighborDesc in pairs(shuffled) do
+				if neighborDesc.Data and neighborDesc.Data.Type ~= RoomType.ROOM_DEFAULT then
+					canPlace = false
+				end
+				if canPlace then
+					local room = level:TryPlaceRoom(roomconf, gridIndex, -1, seed, false, false)
+					if room then
+						RuneRooms:SetRoomSpawnChance(0)
+						return
+					end
+				end
+			end
+		end
+	elseif level:GetAbsoluteStage() ~= LevelStage.STAGE1_1 then
+		RuneRooms:AddToRoomSpawnChance(0.1)
 	end
 end
-RuneRooms:AddCallback(ModCallbacks.MC_PRE_LEVEL_PLACE_ROOM, LevelGen.ReplaceRoom)
-
-RuneRooms:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, function()
-	RuneRooms:AddToRoomSpawnChance(0.1)
-end)
-
+RuneRooms:AddPriorityCallback(ModCallbacks.MC_POST_NEW_LEVEL, CallbackPriority.IMPORTANT, LevelGen.PlaceRoom)
