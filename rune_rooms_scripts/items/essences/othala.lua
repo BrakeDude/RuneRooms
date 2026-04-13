003bdbdcfb7ea553bdee7c24dd4ca1eb327e5061
dpower12 <d.power12@hotmail.it>
2026-04-13 18:17:09 +0200

    Othala first working draft, testing needed.

--- rune_rooms_scripts/items/essences/othala.lua
+++ rune_rooms_scripts/items/essences/othala.lua
@@ -6,43 +6,13 @@ local ITEM_DUPLICATION_CHANCE_PER_LUCK =	0.025
 
 local OthalaItem = RuneRooms.Enums.Item.OTHALA_ESSENCE
 
-local AvoidRecursion = {}
-
----@param item CollectibleType
----@param charge integer
----@param firstTime boolean
----@param slot ActiveSlot
----@param varData integer
----@param player EntityPlayer
-function OthalaEssence:OnItemPickup(item, charge, firstTime, slot, varData, player)
-	if item == CollectibleType.COLLECTIBLE_NULL or nil then return end
-    if item == OthalaItem or TSIL.Collectibles.CollectibleHasFlag(item, ItemConfig.TAG_QUEST)
-    or not firstTime or not TSIL.Collectibles.IsPassiveCollectible(item) or not player:HasCollectible(OthalaItem) then
-		return
-	end
-    local index = TSIL.Players.GetPlayerIndex(player)
-	if AvoidRecursion[index] == item then
-		AvoidRecursion[index] = nil
-		return
-	end
-
-	local rng = player:GetCollectibleRNG(OthalaItem)
-	if rng:RandomFloat() >= ITEM_DUPLICATION_CHANCE then
-		return
-	end
-
-	AvoidRecursion[index] = item
-	player:AddCollectible(item)
-end
-RuneRooms:AddCallback(ModCallbacks.MC_POST_ADD_COLLECTIBLE, OthalaEssence.OnItemPickup)
-
 ---@param pickup EntityPickup
 ---@param collider Entity
 ---@param low boolean
 function OthalaEssence:OnPedestalCollision(pickup, collider, low)
 	if not PlayerManager.AnyoneHasCollectible(OthalaItem) then return end
 	if pickup.SubType == 0 then return end
-	local pData = RuneRooms:GetRerollPersistentData(pickup)
+	local pData = RuneRooms:PickupSave(pickup, true)
 	if pData.RR_OthalaCollectible then return end
 	if pData.RR_OthalaChecked then return end
 	local player = collider:ToPlayer()
@@ -63,17 +33,18 @@ function OthalaEssence:OnPedestalCollision(pickup, collider, low)
 		end, 1, 1, true)
 	end
 end
---RuneRooms:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, OthalaEssence.OnPedestalCollision, PickupVariant.PICKUP_COLLECTIBLE)
+RuneRooms:AddCallback(ModCallbacks.MC_PRE_PICKUP_COLLISION, OthalaEssence.OnPedestalCollision, PickupVariant.PICKUP_COLLECTIBLE)
 
 --- Reroll support for the visuals of the pedestal
 ---@param pickup EntityPickup
 function OthalaEssence:OnPedestalInit(pickup)
-	local pData = RuneRooms:TryGetRerollPersistentData(pickup)
+	local pData = RuneRooms:TryGetPickupSave(pickup, true)
 	if pData and pData.RR_OthalaCollectible then
 		pickup.Color = Color(1, 0, 1, 0.5)
+		pickup.Timeout = 999999999
 	end
 end
---RuneRooms:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, OthalaEssence.OnPedestalInit, PickupVariant.PICKUP_COLLECTIBLE)
+RuneRooms:AddCallback(ModCallbacks.MC_POST_PICKUP_INIT, OthalaEssence.OnPedestalInit, PickupVariant.PICKUP_COLLECTIBLE)
 
 ---@param player EntityPlayer
 ---@return boolean
@@ -85,7 +56,7 @@ function OthalaEssence:RollChance(player)
 end
 
 function OthalaEssence:MarkChecked(pickup)
-	local pData = RuneRooms:GetRerollPersistentData(pickup)
+	local pData = RuneRooms:PickupSave(pickup, true)
 	pData.RR_OthalaChecked = true
 end
 
@@ -96,10 +67,10 @@ function OthalaEssence:MakeOthalaPedestal(ID, pos, pickup)
 	if pickup then
 		pickup:Morph(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, ID)
 	else
-		pickup = Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, ID, pos, Vector.Zero, nil):ToPickup()
+		pickup = Isaac.Spawn(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, ID, pos, Vector.Zero, nil):ToPickup() ---@cast pickup EntityPickup
 	end
 	pickup.Color = Color(1, 0, 1, 0.5)
 	pickup.Timeout = 999999999
-	local pData = RuneRooms:GetRerollPersistentData(pickup)
+	local pData = RuneRooms:PickupSave(pickup, true)
 	pData.RR_OthalaCollectible = true
 end
