8ff3a1bbd2f02914999984110f84c1d4c25840e5
BrakeDude <lockbrake@gmail.com>
2026-04-30 11:35:33 +0300

    Fix EID errors

--- rune_rooms_scripts/mod_compat/eid/main.lua
+++ rune_rooms_scripts/mod_compat/eid/main.lua
@@ -1,71 +1,67 @@
 local Descriptions = RuneRooms.Constants.Descriptions
-local runeSprites = Sprite("gfx/ui/eid_rune_icon.anm2",true)
+local runeSprites = Sprite("gfx/ui/eid_rune_icon.anm2", true)
 
 ---@param runeEffect RuneEffect
 local function GetPositiveRuneEffectDescription(runeEffect)
-    local language = EID:getLanguage()
-    local runeEffectDesc = Descriptions.PositiveRuneEffect[runeEffect][language]
+	local language = EID:getLanguage()
+	local runeEffectDesc = Descriptions.PositiveRuneEffect[runeEffect][language]
 
-    if not runeEffectDesc then
-        runeEffectDesc = Descriptions.PositiveRuneEffect[runeEffect].en_us
-    end
+	if not runeEffectDesc then
+		runeEffectDesc = Descriptions.PositiveRuneEffect[runeEffect].en_us
+	end
 
-    return runeEffectDesc
+	return runeEffectDesc
 end
-
 
 ---@param runeEffect RuneEffect
 local function GetNegativeRuneEffectDescription(runeEffect)
-    local language = EID:getLanguage()
-    local runeEffectDesc = Descriptions.NegativeRuneEffect[runeEffect][language]
+	local language = EID:getLanguage()
+	local runeEffectDesc = Descriptions.NegativeRuneEffect[runeEffect][language]
 
-    if not runeEffectDesc then
-        runeEffectDesc = Descriptions.NegativeRuneEffect[runeEffect].en_us
-    end
+	if not runeEffectDesc then
+		runeEffectDesc = Descriptions.NegativeRuneEffect[runeEffect].en_us
+	end
 
-    return runeEffectDesc
+	return runeEffectDesc
 end
 
 ---@param giantCrystal Entity
 local function SpawnNegativeEffectDescriptionHolder(giantCrystal)
-    local runeEffect = RuneRooms:GetRuneEffectForFloor()
+	local runeEffect = RuneRooms:GetRuneEffectForFloor()
 
-    local runeEffectDesc = GetNegativeRuneEffectDescription(runeEffect)
-    if not runeEffectDesc then return end
+	local runeEffectDesc = GetNegativeRuneEffectDescription(runeEffect)
+	if not runeEffectDesc then
+		return
+	end
 
-    local eidHolder = TSIL.EntitySpecific.SpawnEffect(
-        RuneRooms.Enums.EffectVariant.EID_DESCRIPTION_HOLDER,
-        0,
-        giantCrystal.Position
-    )
+	local eidHolder =
+		TSIL.EntitySpecific.SpawnEffect(RuneRooms.Enums.EffectVariant.EID_DESCRIPTION_HOLDER, 0, giantCrystal.Position)
 
-    eidHolder:GetData().EID_Description = {
-        Name = runeEffectDesc.name,
-        Description = runeEffectDesc.description
-    }
+	eidHolder:GetData().EID_Description = {
+		Name = runeEffectDesc.name,
+		Description = runeEffectDesc.description,
+	}
 end
 
+EID.effectList[tostring(RuneRooms.Enums.EffectVariant.EID_DESCRIPTION_HOLDER)] = true
 
-RuneRooms:AddModCompat("EID", function ()
-    EID.effectList[tostring(RuneRooms.Enums.EffectVariant.EID_DESCRIPTION_HOLDER)] = true
+-- Collectibles
+for collectible, translations in pairs(Descriptions.Collectibles) do
+	for language, description in pairs(translations) do
+		EID:addCollectible(collectible, description.description, description.name, language)
+	end
+end
 
-    -- Collectibles
-    for collectible, translations in pairs(Descriptions.Collectibles) do
-        for language, description in pairs(translations) do
-            EID:addCollectible(collectible, description.description, description.name, language)
-        end
-    end
+-- Runes
+for rune, translations in pairs(Descriptions.Runes) do
+	for language, description in pairs(translations) do
+		EID:addCard(rune, description.description, description.name, language)
+	end
+	EID:addIcon("Card" .. rune, "Runes", 0, 12, 12, 0, 0, runeSprites)
+	EID:AddIconToObject(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_TAROTCARD, rune, "Card" .. rune)
+end
 
-    -- Runes
-    for rune, translations in pairs(Descriptions.Runes) do
-        for language, description in pairs(translations) do
-            EID:addCard(rune, description.description, description.name, language)
-        end
-        EID:addIcon("Card"..rune, "Runes", 0, 12, 12, 0, 0, runeSprites)
-        EID:AddIconToObject(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_TAROTCARD, rune, "Card"..rune)
-    end
-
-    --[[RuneRooms:AddCallback(
+--[[RuneRooms:AddCallback(
         RuneRooms.Enums.CustomCallback.POST_GENERIC_PROP_INIT,
         function (_, giantCrystal)
             if RuneRooms:IsGiantRuneCrystalBroken(giantCrystal) then
@@ -91,4 +87,3 @@ RuneRooms:AddModCompat("EID", function ()
             SpawnNegativeEffectDescriptionHolder(giantCrystal)
         end
     )]]
-end)
