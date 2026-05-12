2560197078694b8e8294c0040fc805dca99b54eb
BrakeDude <lockbrake@gmail.com>
2026-05-12 14:47:35 +0300

    Update MinimapAPI to 2.59 and SaveManager to 2.4a

--- scripts/minimapapi/version.lua
+++ scripts/minimapapi/version.lua
@@ -2,4 +2,4 @@ local MinimapAPI = require("scripts.minimapapi")
 
 MinimapAPI.BranchVersion = RuneRooms.Constants.MINIMAPI_VERSION --branch
 MinimapAPI.MajorVersion = 2
-MinimapAPI.MinorVersion = 58
+MinimapAPI.MinorVersion = 59
